9051df6a67bffd6b061aad5da70e33ff4d6e9be0
Nathan Chong <nathan.chong@gmail.com>
2014-12-09 13:18:09 +0000

    Script for generating Tyler buckets

---
+++ bucket.sh
@@ -0,0 +1,25 @@
+BUCKET=c11popl15/c11
+
+CATS=${BUCKET}/cats
+mkdir -p ${CATS}
+
+# generate all models
+cp library.cat c11popl15/c11/cats
+for RF in ConsRFna Naive Arf Arfna; do
+  for SC in SCorig SCnew; do
+    for RS in RSorig RSnew; do
+      for ST in STorig STnew; do
+      ./genmodel.py --RF $RF --SC $SC --RS $RS --ST $ST --output ${CATS}/${RF}_${SC}_${RS}_${ST}.cat;
+      done;
+    done;
+  done;
+done
+
+TESTS=${BUCKET}/tests
+mkdir -p ${TESTS}
+
+cd tests
+cp *.litmus ../${TESTS}
+for f in *.litmus-template; do
+  ./genlitmus.py --input $f --output_base ../${TESTS}/Z_${f/.litmus-template};
+done
